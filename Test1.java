fadf0ac90ad723445886b0b8077af36e2f7110cb
maika <zhouxinwei333@163.com>
2020-01-29 21:00:48 +0800

    Commit Test1.java once again

--- Test1.java
+++ Test1.java
@@ -4,7 +4,8 @@ public class Test1
 	public static void main(String[] args)
 	{
 		System.out.println("Hello GIT");
-
+		System.out.println("Hello Maika");
+		System.out.println("Hello Alreen");
 	}
 
 }
